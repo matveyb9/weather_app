6f66f0f4aa1d03d0cc180a7918e0032375b5fdac
matveyb9 <bmb20109@yandex.ru>
2026-03-11 23:48:04 +0600

    Update the build configuration: add support for subscription keys and update the Gradle version to 8.13

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,16 @@
 import java.util.Properties
+import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keyProperties = Properties()
+val keyPropertiesFile = rootProject.file("key.properties")
+if (keyPropertiesFile.exists()) {
+    keyProperties.load(FileInputStream(keyPropertiesFile))
 }
 
 // Read flutter.versionCode / flutter.versionName from local.properties
@@ -44,10 +51,19 @@ android {
         versionName = flutterVersionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias     = keyProperties["keyAlias"]     as String
+            keyPassword  = keyProperties["keyPassword"]  as String
+            storeFile    = file(keyProperties["storeFile"] as String)
+            storePassword = keyProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: replace with a production keystore before publishing
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             isShrinkResources = false
         }
